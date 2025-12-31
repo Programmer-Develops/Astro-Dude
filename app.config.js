8d541639e8210527232cc1a8d0d3b2651705ab63
Programmer-Develops <shachipandya29@gmail.com>
2025-12-31 13:33:42 +0530

    FIX: fixed chatbot configuration (API key error)

--- app.config.js
+++ app.config.js
@@ -1,6 +1,17 @@
 module.exports = ({ config }) => ({
   ...config,
+  name: "Astro Dude",
+  slug: "adude",
+  version: "1.0.0",
+  android: {
+    package: "com.shantanupandya.adude",
+    versionCode: 1,
+    playStoreUrl: "https://play.google.com/store/apps/details?id=com.shantanupandya.adude"
+  },
   extra: {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY ?? '',
+    eas: {
+      projectId: "6045806b-cf62-4d4f-a1e0-aecbaf7b1699"
+    }
   },
 });
